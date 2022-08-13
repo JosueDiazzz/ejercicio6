1e7ce6b25eebcc083aa32cb8e9a4fdf9a57e8de0
JOSUE RAFAEL GOMEZ DIAZ <SMSS108122@ugb.edu.sv>
2022-08-13 15:30:57 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            //Captura de datos desde consola
+            Console.WriteLine("introduzca un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca otro numero");
+            num2 = int.Parse(Console.ReadLine());
+            // llamado de funciones locales
+            Console.WriteLine("la suma es: " + Suma(num1, num2));
+            Console.WriteLine("la resta es: " + resta(num1, num2));
+            Console.WriteLine("la multiplicacion es: " + Multi(num1, num2));
+            Console.WriteLine(" la division es: " + Div(num1, num2));
+
+             }
+
+
+
+
+            
+               
+
+                
+        }
+
+       
+    }
+}
